88f7bcf492cdc311f7cb1c1e50f57ab9b3312adb
Thanh Tan Tran <trantanthanh9a10@gmail.com>
2024-07-07 15:35:58 +0700

    stats viewer api and auto save viewers

--- app/models/index.js
+++ app/models/index.js
@@ -5,5 +5,6 @@ import Notification from './Notification.js';
 import Follower from './Follower.js';
 import History from './History.js';
 import Banned from './Banned.js';
+import StatsViewer from './StatsViewer.js';
 
-export { User, Stream, Chat, Notification, Follower, History, Banned };
+export { User, Stream, Chat, Notification, Follower, History, Banned, StatsViewer };
