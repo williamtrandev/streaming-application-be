d7c9fa9ffe4b3469f67bc73c4137bb6dec2121e2
Thanh Tan Tran <trantanthanh9a10@gmail.com>
2024-07-19 22:55:07 +0700

    change cors config

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ dotenv.config();
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '10mb' }));
-app.use(cors());
+app.use(cors(
+	{
+		origin: [process.env.ORIGIN]
+	}
+));
 app.use(morgan('dev'));
 // app.use(express.static(path.join(__dirname, "public")));
 
